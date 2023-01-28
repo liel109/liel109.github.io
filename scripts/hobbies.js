dca8c74b51090bcf4f0ec89c04939fba9abe7c54
Liel Huri <liel.huri@post.runi.ac.il>
2023-01-28 17:13:57 +0200

    All but Contact page is finished

--- scripts/hobbies.js
+++ scripts/hobbies.js
@@ -1,5 +1,6 @@
 "use strict";
 
+let currnet_section = NaN;
 //define sections
 let gaming_section = document.getElementById("gaming");
 let coding_section = document.getElementById("coding");
@@ -19,7 +20,13 @@ let dict = {"gaming" : 0, "coding" : 1, "sports" : 2, "guitar" : 3};
 function show_section(sect){
     for(let i=0; i<sections.length; i++){
         if(dict[sect] == i) {
-            sections[i].style.display = "block";
+            currnet_section = i;
+            if(window.innerWidth <= 640) {
+                sections[i].style.display = "flex";
+            }
+            else{
+                sections[i].style.display = "block";
+            }
             buttons[i].style.transform = "translateY(-20px)";
         }
         else {
@@ -28,3 +35,12 @@ function show_section(sect){
         }
     }
 }
+
+window.addEventListener("resize", ()=>{
+    if(window.innerWidth <= 640) {
+        sections[currnet_section].style.display = "flex";
+    }
+    else{
+        sections[currnet_section].style.display = "block";
+    }
+});
